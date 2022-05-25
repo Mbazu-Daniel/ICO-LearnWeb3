280ce98c796d5d896b79dede8b5eace11238aada
Mbazu Daniel <mbazudaniel97@gmail.com>
2022-05-25 03:06:40 +0100

    add comment on contracts

--- hardhat-tutorial/contracts/ICryptoDevs.sol
+++ hardhat-tutorial/contracts/ICryptoDevs.sol
@@ -1,8 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 interface ICryptoDevs {
-    function tokenOfOwnerByIndex(address owner, uint index) external view returns(uint tokenId);
+    /**
+     * @dev Returns a token ID owned by `owner` at a given `index` of its token list.
+     * Use along with {balanceOf} to enumerate all of ``owner``'s tokens.
+     */
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        external
+        view
+        returns (uint256 tokenId);
 
-    function balanceOf(address owner) external view returns (uint balance);
+    /**
+     * @dev Returns the number of tokens in ``owner``'s account.
+     */
+    function balanceOf(address owner) external view returns (uint256 balance);
 }
